f8136b7db63e011dd44513cb372a4b74de617786
takumiookubo <tsukuba901149@gmail.com>
2017-06-22 08:13:28 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -17,6 +19,10 @@ class UsersController < ApplicationController
        render :new
     end
   end
+  
   private
+  
+  def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end  
 end  
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  before_save {self.email.downcase! }
+  before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
