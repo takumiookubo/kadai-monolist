39ab9083686161b2930ec639174ee037c0d7b974
takumiookubo <tsukuba901149@gmail.com>
2017-06-23 12:25:04 +0000

    rakuten api

---
+++ db/migrate/20170623080700_create_items.rb
@@ -0,0 +1,13 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :code
+      t.string :name
+      t.string :string
+      t.string :url
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
